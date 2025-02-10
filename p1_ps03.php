e70c5ebc75e680c339895e9a03b25d9f4a68da78
Diane Nicole R. Antonio <202280159@psu.palawan.edu.ph>
2025-02-10 21:37:39 +0800

    Add array of even numbers display to Part I - Practice Set 03

--- p1_ps03.php
+++ p1_ps03.php
@@ -6,6 +6,31 @@
     <title>Part I - Practice Set 03</title>
 </head>
 <body>
-    
+    <h2>Array of Even Numbers</h2>
+    <?php
+        $matrix = [
+            [12, 23, 34],
+            [45, 55, 62],
+            [71, 84, 90]
+        ];
+
+        $i = 0;
+        $j = 0;
+        $rows = count($matrix);
+        $cols = count($matrix[0]);
+
+        echo "<ul>";
+        while ($i < $rows) {
+            while ($j < $cols) {
+                if ($matrix[$i][$j] % 2 == 0) {
+                    echo "<li>" . $matrix[$i][$j] . "</li>";
+                }
+                $j++;
+            }
+            $j = 0; // Reset Column
+            $i++;
+        }
+        echo "</ul>";
+    ?>
 </body>
 </html>
